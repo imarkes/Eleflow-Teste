4b474ce0e908ea44c1875744511bb03090cb3649
Ivan Marques <i.markes@hotmail.com>
2022-03-09 08:29:46 -0300

    Classes criadas

---
+++ air_cia.py
@@ -0,0 +1,55 @@
+import csv
+import glob
+import os
+
+path = 'AIR_CIA'
+
+class FilesAIRCIA:
+    def __init__(self, path) -> None:
+        self.path = path
+        self.csv_files = glob.glob(os.path.join(path, '*.csv'))
+
+    def normalize_readers_csv(self, filename):
+        """
+        :param: ['Ivan Marques', Desenvolvedor Python]
+        :return: ['ivan_marques', desenvolvedor_python]
+        """
+
+        with open(filename, newline='', encoding='utf-8') as csvfile:
+            reader = csv.DictReader(csvfile, delimiter=';')
+
+            for row in reader:
+                headers = list(row)
+                ICAO = headers[1].split()[0].lower()
+                IATA = headers[1].split()[1].lower()
+                new_header = [head.replace(' ', '_').lower() for head in headers]  #  converte em snak_case
+                new_header[1] = ICAO
+                new_header.insert(2, IATA)
+                headers = new_header
+
+                new_values = list(row.values())
+                values = new_values[1].split() 
+                if len(values)==0:
+                    new_values[1] = None
+                else:
+                    new_values[1] = values[0]
+    
+                if len(values) <= 1:
+                    new_values.insert(2, None)
+                else:
+                    new_values.insert(2, values[1])
+
+                new_dict = dict(zip(headers, new_values))    
+              
+                return new_dict
+
+    def read_all_files_in_path(self):
+        """Read files."""
+
+        for files in self.csv_files:
+            yield self.normalize_readers_csv(files)
+
+   
+if __name__ == '__main__':
+    a = FilesAIRCIA(path=path)
+    print(list(a.read_all_files_in_path()))
---
+++ vra.py
@@ -0,0 +1,48 @@
+import glob
+import json
+from msilib.schema import Patch
+import os
+import re
+
+path = 'VRA'
+
+class FilesVRA:
+    def __init__(self, path) -> None:
+        self.path = path
+        self.vra_files_json = glob.glob(os.path.join(path, '*.json'))
+
+    def convert_words_for_snake_case(self, words: str) -> str:
+        """
+        :param: IvanMarques
+        :return: ivan_marques
+        """
+        assert isinstance(words, str)
+
+        words = re.findall('[A-Z]*[^A-Z]*', words)  # split letras maiúsculas
+        words = [x.lower() for x in words]
+
+        return '_'.join(words)[:-1]
+
+    def normalizer_headers(self, file_json: list) -> list:
+        """
+        :param: [IvanMarques, DevPython]
+        :return: [ivan_marques, dev_python]
+        """
+
+        with open(file_json, encoding='utf-8-sig') as f_json:
+            read_data = json.load(f_json)
+
+            for data in read_data:
+                headers = list(data)
+                new_header = [self.convert_words_for_snake_case(head) for head in headers]
+                return new_header
+            
+    def read_all_files_in_path(self):
+        """Return generator object FilesVRA."""
+        for files in self.vra_files_json:
+            yield self.normalizer_headers(files)
+
+
+if __name__ == '__main__':
+    a = FilesVRA(path)
+    print(list(a.read_all_files_in_path()))
