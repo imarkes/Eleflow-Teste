2eabd3ab6060697b8f5d2bd73232bb152f24f399
Ivan Marques <i.markes@hotmail.com>
2022-03-10 09:27:23 -0300

    feat: classes main ok.

---
+++ main.py
@@ -0,0 +1,19 @@
+from data_engineer_test.aerodromos import AerodromosCSV
+from data_engineer_test.air_cia import FilesAIRCIA
+from data_engineer_test.vra import FilesVRA
+
+vra = FilesVRA()
+vra.read_all_files_in_path()
+vra.create_dir_new_files('FilesVRA')
+vra.generate_file_json()
+
+air = FilesAIRCIA()
+air.read_all_files_in_path()
+air.create_dir_new_files('FilesAirCia')
+air.generate_csv_air_cia()
+
+icao = AerodromosCSV()
+icao.get_codgos_icao('FilesVRA/vra.json')
+icao.response_get_url()
+icao.create_dir_new_files('Aerodromos')
+icao.generate_csv()
--- vra.py
+++ vra.py
@@ -29,30 +29,52 @@ class FilesVRA:
         :return: [ivan_marques, dev_python]
         """
 
-        with open(file_json, encoding='utf-8-sig') as f_json:
-            read_data = json.load(f_json)
+        try:
+            with open(file_json, encoding='utf-8-sig') as f_json:
+                read_data = json.load(f_json)
 
-            for data in read_data:
-                headers = list(data)
-                new_header = [self.convert_words_for_snake_case(words) for words in headers]
+                for data in read_data:
+                    headers = list(data)
+                    new_header = [self.convert_words_for_snake_case(words) for words in headers]
 
-                values = list(data.values())
-                new_json = dict(zip(new_header, values))
-                yield new_json
+                    values = list(data.values())
+                    new_json = dict(zip(new_header, values))
+                    yield new_json
+
+        except Exception as e:
+            print(f'Não foi possivel normalizar os dados. Error: {e}')
 
     def read_all_files_in_path(self):
-        """Return generator object FilesVRA."""
-        for files in self.vra_files_json:
-            self.all_files = list(self.normalizer_headers(files))
+        """Return all files normalized."""
+        try:
+            for files in self.vra_files_json:
+                self.all_files = list(self.normalizer_headers(files))
+                return self.all_files
+
+        except Exception as e:
+            print(f'Não foi possivel ler o diretório dados. Error: {e}')
 
     def generate_file_json(self):
         """Generate json file."""
-        with open('vra.json', mode='w', encoding='utf-8') as outfile:
-            json.dump(self.all_files, outfile, ensure_ascii=False, indent=4)
+        try:
+            with open('vra.json', mode='w', encoding='utf-8') as outfile:
+                json.dump(self.all_files, outfile, ensure_ascii=False, indent=4)
+
+        except Exception as e:
+            print(f'Não foi possivel gerar o arquivo Json. Error: {e}')
+
+    def create_dir_new_files(self, path):
+        """Create dir path new files."""
+        assert isinstance(path, str)
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+        os.chdir(path)
 
 
 if __name__ == '__main__':
-    a = FilesVRA()
-    a.read_all_files_in_path()
-    a.generate_file_json()
-
+    ...
+    # a = FilesVRA()
+    # a.read_all_files_in_path()
+    # a.create_dir_new_files('FilesVRA')
+    # a.generate_file_json()
