02a90b0a6496750d1893d6a164f439072b09125e
Ivan Marques <i.markes@hotmail.com>
2022-03-10 09:27:23 -0300

    feat: classes main ok.

--- aerodromos.py
+++ aerodromos.py
@@ -1,4 +1,7 @@
 import csv
+import json
+import os
+
 import requests
 from data_engineer_test.vra import FilesVRA
 
@@ -9,54 +12,81 @@ class AerodromosCSV:
     def __init__(self):
         self.codgos = None
         self.response = []
-        self.result_VRA = FilesVRA()
-        self.list_data_VRA = list(self.result_VRA.read_all_files_in_path())
+        # self.result_VRA = FilesVRA()
+        # self.list_data_VRA = self.result_VRA.read_all_files_in_path()
 
-    def get_codgos_icao(self):
+    def get_codgos_icao(self, path_file):
         """Return codgos ICAO to file VRA."""
-        cods_icao_origem = [cod['icaoaeródromo_origem'] for cod in self.list_data_VRA]
-        cods_icao_destino = [cod['icaoaeródromo_destino'] for cod in self.list_data_VRA]
-        self.codgos = set(cods_icao_origem + cods_icao_destino)
-        return self.codgos
+        try:
+            with open(path_file, 'r', encoding='utf-8') as file:
+                list_data_VRA = json.load(file)
+
+            cods_icao_origem = [cod['icaoaeródromo_origem'] for cod in list_data_VRA]
+            cods_icao_destino = [cod['icaoaeródromo_destino'] for cod in list_data_VRA]
+            self.codgos = set(cods_icao_origem + cods_icao_destino)  # Remove codgos repetidos.
+            return self.codgos
+
+        except Exception as e:
+            print(f'Não foi possivel obter os codgos_ICAO.. Erro: {e}')
 
     def response_get_url(self):
         """Get data URL."""
-        url = "https://airport-info.p.rapidapi.com/airport"
-        headers = {
-            'x-rapidapi-host': "airport-info.p.rapidapi.com",
-            'x-rapidapi-key': "4e66b5eee7mshe07cecc516f5f82p1d7870jsnece9b9399d9e"
-        }
+        try:
+            url = "https://airport-info.p.rapidapi.com/airport"
+            headers = {
+                'x-rapidapi-host': "airport-info.p.rapidapi.com",
+                'x-rapidapi-key': "4e66b5eee7mshe07cecc516f5f82p1d7870jsnece9b9399d9e"
+            }
 
-        for cod in self.codgos:
-            querystring = {"icao": cod}
-            resp = requests.request("GET", url, headers=headers, params=querystring).json()
-            self.response.append(resp)
+            for cod in self.codgos:
+                querystring = {"icao": cod}
+                resp = requests.request("GET", url, headers=headers, params=querystring).json()
+
+                # valida o codgo icao.
+                if not 'error' in resp:
+                    self.response.append(resp)
+
+            return self.response
+
+        except Exception as e:
+            print(f'Não foi possivel obter os dados da requisição HTTP.. Erro: {e}')
 
     def generate_csv(self):
         """Generate CSV File."""
-        with open('aerodromos.csv', mode='w', newline='', encoding='utf-8') as file:
-            fieldnames = list(self.response[0].keys())
+        try:
+            with open('aerodromos.csv', mode='w', newline='', encoding='utf-8') as file:
+                fieldnames = list(self.response[0].keys())
 
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                writer.writeheader()
 
-            for row in range(len(self.response)):
+                for row in range(len(self.response)):
 
-                writer.writerow(
-                    {'id': self.response[row]['id'], 'iata': self.response[row]['iata'],
-                     'icao': self.response[row]['icao'],'name': self.response[row]['name'],
-                     'location': self.response[row]['location'], 'street_number': self.response[row]['street_number'],
-                     'street': self.response[row]['street'],'city': self.response[row]['city'],
-                     'county': self.response[row]['county'],'state': self.response[row]['state'],
-                     'country_iso': self.response[row]['country_iso'],'country': self.response[row]['country'],
-                     'postal_code': self.response[row]['postal_code'],'phone': self.response[row]['phone'],
-                     'latitude': self.response[row]['latitude'],'longitude': self.response[row]['longitude'],
-                     'uct': self.response[row]['uct'],'website': self.response[row]['website']}
-                )
+                    writer.writerow(
+                        {'id': self.response[row]['id'], 'iata': self.response[row]['iata'],
+                         'icao': self.response[row]['icao'], 'name': self.response[row]['name'],
+                         'location': self.response[row]['location'],
+                         'street_number': self.response[row]['street_number'],
+                         'street': self.response[row]['street'], 'city': self.response[row]['city'],
+                         'county': self.response[row]['county'], 'state': self.response[row]['state'],
+                         'country_iso': self.response[row]['country_iso'], 'country': self.response[row]['country'],
+                         'postal_code': self.response[row]['postal_code'], 'phone': self.response[row]['phone'],
+                         'latitude': self.response[row]['latitude'], 'longitude': self.response[row]['longitude'],
+                         'uct': self.response[row]['uct'], 'website': self.response[row]['website']}
+                    )
+
+        except Exception as e:
+            print(f'Não foi possivel gerar o arquivo CSV.. Erro: {e}')
+
+    def create_dir_new_files(self, path):
+        """Create dir path new files."""
+        assert isinstance(path, str)
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+        os.chdir(path)
 
 
 if __name__ == '__main__':
-    a = AerodromosCSV()
-    a.get_codgos_icao()
-    a.response_get_url()
-    a.generate_csv()
+    ...
+
--- air_cia.py
+++ air_cia.py
@@ -14,66 +14,83 @@ class FilesAIRCIA:
         :param: ['Ivan Marques', Desenvolvedor Python]
         :return: ['ivan_marques', desenvolvedor_python]
         """
+        try:
+            with open(filename, newline='', encoding='utf-8') as csvfile:
+                reader = csv.DictReader(csvfile, delimiter=';')
 
-        with open(filename, newline='', encoding='utf-8') as csvfile:
-            reader = csv.DictReader(csvfile, delimiter=';')
+                for row in reader:
+                    headers = list(row)
+                    ICAO = headers[1].split()[0].lower()
+                    IATA = headers[1].split()[1].lower()
+                    new_header = [head.replace(' ', '_').lower() for head in headers]  # converte em snak_case
+                    new_header[1] = ICAO
+                    new_header.insert(2, IATA)
+                    headers = new_header
 
-            for row in reader:
-                headers = list(row)
-                ICAO = headers[1].split()[0].lower()
-                IATA = headers[1].split()[1].lower()
-                new_header = [head.replace(' ', '_').lower() for head in headers]  # converte em snak_case
-                new_header[1] = ICAO
-                new_header.insert(2, IATA)
-                headers = new_header
+                    new_values = list(row.values())
+                    values = new_values[1].split()
+                    if len(values) == 0:
+                        new_values[1] = None
+                    else:
+                        new_values[1] = values[0]
 
-                new_values = list(row.values())
-                values = new_values[1].split()
-                if len(values) == 0:
-                    new_values[1] = None
-                else:
-                    new_values[1] = values[0]
+                    if len(values) <= 1:
+                        new_values.insert(2, None)
+                    else:
+                        new_values.insert(2, values[1])
 
-                if len(values) <= 1:
-                    new_values.insert(2, None)
-                else:
-                    new_values.insert(2, values[1])
+                    new = dict(zip(headers, new_values))
 
-                new = dict(zip(headers, new_values))
+                    self.new_dict.append(new)
 
-                self.new_dict.append(new)
-                return self.new_dict
+            return self.new_dict
+
+        except Exception as e:
+            print(f'Não foi possível Normalizar os dados. Error: {e}')
 
     def read_all_files_in_path(self):
         """Return generator object FilesAIRCIA."""
-        print(self.new_dict)
-        for files in self.path_csv_files:
-            self.normalize_readers_csv(files)
+        try:
+            for files in self.path_csv_files:
+                self.normalize_readers_csv(files)
+
+        except Exception as e:
+            print(f'Não foi possível ler o diretório. Error: {e}')
 
     def generate_csv_air_cia(self):
         """Generate CSV File."""
-        with open('air_cia.csv', mode='w', newline='', encoding='utf-8') as file:
-            fieldnames = list(self.new_dict[0].keys())
+        try:
+            with open('air_cia.csv', mode='w', newline='', encoding='utf-8') as file:
+                fieldnames = list(self.new_dict[0].keys())
+                writer = csv.DictWriter(file, fieldnames=fieldnames)
+                writer.writeheader()
 
-            writer = csv.DictWriter(file, fieldnames=fieldnames)
-            writer.writeheader()
+                for row in self.new_dict:
 
-            for row in self.new_dict:
+                    writer.writerow({
+                        'razão_social': row['razão_social'], 'icao': row['icao'],
+                        'iata': row['iata'], 'cnpj': row['cnpj'], 'atividades_aéreas': row['atividades_aéreas'],
+                        'endereço_sede': row['endereço_sede'], 'telefone': row['telefone'], 'e-mail': row['e-mail'],
+                        'decisão_operacional': row['decisão_operacional'],
+                        'data_decisão_operacional': row['data_decisão_operacional'],
+                        'validade_operacional': row['validade_operacional']
+                    })
+        except Exception as e:
+            print(f'Não foi possível gerar o arquivo CSV. Error: {e}')
 
-                writer.writerow({
-                    'razão_social': row['razão_social'], 'icao': row['icao'],
-                    'iata': row['iata'], 'cnpj': row['cnpj'], 'atividades_aéreas': row['atividades_aéreas'],
-                    'endereço_sede': row['endereço_sede'], 'telefone': row['telefone'], 'e-mail': row['e-mail'],
-                    'decisão_operacional': row['decisão_operacional'],
-                    'data_decisão_operacional': row['data_decisão_operacional'],
-                    'validade_operacional': row['validade_operacional']
-                })
+    def create_dir_new_files(self, path):
+        """Create dir path new files."""
+        assert isinstance(path, str)
+
+        if not os.path.exists(path):
+            os.makedirs(path)
+        os.chdir(path)
 
 
 if __name__ == '__main__':
-    a = FilesAIRCIA()
-    a.read_all_files_in_path()
-    a.generate_csv_air_cia()
+    ...
+    # a = FilesAIRCIA()
+    # a.read_all_files_in_path()
+    # a.create_dir_new_files('FilesAirCia')
+    # a.generate_csv_air_cia()
 
-# 'razão_social':, 'icao':, 'iata':, 'cnpj':, 'atividades_aéreas':, 'endereço_sede':, 'telefone':, 'e-mail':,
-# 'decisão_operacional':, 'data_decisão_operacional':, 'validade_operacional':
